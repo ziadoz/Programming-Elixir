2ef0d8551d34c6f6de3503f5b8845e9557d78d68
Jamie York <ziadoz@gmail.com>
2016-06-22 20:07:00 +0100

    Scopes

---
+++ chapter4/scopes.exs
@@ -0,0 +1,30 @@
+# With Expression Scope
+import System
+
+content = "Now is the time"
+
+lp = with {:ok, file}    = File.open(Path.join([cwd(), "passwd"])),
+           content       = IO.read(file, :all), # Different Scope
+           :ok           = File.close(file),
+           [_, uid, gid] = Regex.run(~r/_lp:.*?:(\d+):(\d+)/, content)
+    do
+           "Group: #{gid}, User: #{uid}"
+    end
+
+IO.puts lp
+IO.puts content
+
+# Match
+IO.puts with [a|_] <- [1, 2, 3], do: a
+IO.inspect with [a|_] <- nil, do: a
+
+# With Expression Match
+result = with {:ok, file}     = File.open(Path.join([cwd(), "passwd"])),
+              content        = IO.read(file, :all), # Different Scope
+              :ok            = File.close(file),
+              [_, uid, gid] <- Regex.run(~r/xxx:.*?:(\d+):(\d+)/, content)
+        do
+              "Group: #{gid}, User: #{uid}"
+        end
+
+IO.inspect result
