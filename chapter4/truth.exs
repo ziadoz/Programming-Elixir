d49a4596ca7978e5292a76635d04b14ae3b9e555
Jamie York <ziadoz@gmail.com>
2016-06-22 16:30:30 +0100

    Complete truth

--- chapter4/truth.exs
+++ chapter4/truth.exs
@@ -7,3 +7,72 @@ IO.inspect nil
 IO.puts true === :true
 IO.puts false === :false
 IO.puts nil === :nil
+
+# Operators
+a = 1
+b = 1.0
+IO.puts "a = #{a} and b = #{b}"
+
+IO.puts "a === b"
+IO.puts a === b # Strict Equality
+
+IO.puts "a !== b"
+IO.puts a !== b # Strict Inequality
+
+IO.puts "a == b"
+IO.puts a == b  # Value Equality
+
+IO.puts "a != b"
+IO.puts a != b  # Value Inequality
+
+# Comparisons
+IO.puts "a > b"
+IO.puts a > b
+
+IO.puts "a >= b"
+IO.puts a >= b
+
+IO.puts "a < b"
+IO.puts a < b
+
+IO.puts "a <= b"
+IO.puts a <= b
+
+# Boolean Operators
+a = true
+b = false
+IO.puts "a = #{a} and b = #{b}"
+
+IO.puts "a or b"
+IO.puts a or b
+
+IO.puts "a and b"
+IO.puts a and b
+
+IO.puts "not a"
+IO.puts not a
+
+# Relaxed Boolean Operators
+a = 1
+b = 1.0
+IO.puts "a = #{a} and b = #{b}"
+
+IO.puts "a || b"
+IO.puts a || b
+
+IO.puts "a && b"
+IO.puts a && b
+
+IO.puts "!a"
+IO.puts !a
+
+# Arithmetic Operators
+a = 6.8
+b = 1
+IO.puts "a = #{a} and b = #{b}"
+
+IO.puts "a + b = #{a + b}"
+IO.puts "a - b = #{a - b}"
+IO.puts "a / b = #{a / b}"
+IO.puts "a * b = #{a * b}"
+IO.puts "remainder of 11 / 3 = #{rem(11, 3)}"
