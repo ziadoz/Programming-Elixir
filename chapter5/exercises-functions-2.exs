0e474c269640d5660f1614fc6da8959f16be9694
Jamie York <ziadoz@gmail.com>
2016-07-04 08:12:45 +0100

    Exercises Functions 2

---
+++ chapter5/exercises-functions-2.exs
@@ -0,0 +1,12 @@
+# FizzBuzz
+fizzbuzz = fn
+    0, 0, _ -> "FizzBuzz"
+    0, _, _ -> "Fizz"
+    _, 0, _ -> "Buzz"
+    _, _, x -> x
+end
+
+IO.puts fizzbuzz.(0, 0, 0)
+IO.puts fizzbuzz.(0, 1, 0)
+IO.puts fizzbuzz.(1, 0, 0)
+IO.puts fizzbuzz.(1, 1, 1)
