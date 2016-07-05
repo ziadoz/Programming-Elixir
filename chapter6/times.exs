ca29aa8aa633f7048646c54855d57718880d7fea
Jamie York <ziadoz@gmail.com>
2016-07-05 19:50:02 +0100

    Times Exercises

--- chapter6/times.exs
+++ chapter6/times.exs
@@ -1,5 +1,13 @@
 defmodule Times do
+    # Multi Liner
     def double(n) do
         n * 2
+    end
+
+    # One Liner
+    def triple(n), do: n * 3
+
+    def quaduple(n) do
+        double(n) * 2
     end
 end
